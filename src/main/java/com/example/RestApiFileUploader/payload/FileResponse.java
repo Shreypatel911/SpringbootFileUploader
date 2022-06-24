9aa65b2763bd8cb548f99f5900fdc5dbdaf326e4
Shreypatel911 <shreypatel11999@gmail.com>
2022-06-25 04:27:55 +0530

    file uploading feature implemented

---
+++ src/main/java/com/example/RestApiFileUploader/payload/FileResponse.java
@@ -0,0 +1,28 @@
+package com.example.RestApiFileUploader.payload;
+
+public class FileResponse {
+
+    private String name;
+    private String message;
+
+    public FileResponse(String name, String message) {
+        this.name = name;
+        this.message = message;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
--- src/main/java/com/example/RestApiFileUploader/services/FileService.java
+++ src/main/java/com/example/RestApiFileUploader/services/FileService.java
@@ -2,7 +2,9 @@ package com.example.RestApiFileUploader.services;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 public interface FileService {
 
-    String fileUpload(String path, MultipartFile file);
+    String fileUpload(String path, MultipartFile file) throws IOException;
 }
